edc435f2b02ed38c010c8c323be7f45f514433b0
Moren YANG <morenyang88@gmail.com>
2018-07-25 15:23:02 +0800

    setup env

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -11,6 +10,8 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
+const HappyPack = require('happypack');
+const threadPool = HappyPack.ThreadPool({ size: require('os').cpus().length });
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
@@ -64,8 +65,7 @@ module.exports = {
     // This is the URL that app is served from. We use "/" in development.
     publicPath: publicPath,
     // Point sourcemap entries to original disk location (format as URL on Windows)
-    devtoolModuleFilenameTemplate: info =>
-      path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+    devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
@@ -84,7 +84,6 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -110,16 +109,7 @@ module.exports = {
       {
         test: /\.(js|jsx|mjs)$/,
         enforce: 'pre',
-        use: [
-          {
-            options: {
-              formatter: eslintFormatter,
-              eslintPath: require.resolve('eslint'),
-              
-            },
-            loader: require.resolve('eslint-loader'),
-          },
-        ],
+        use: 'happypack/loader?id=eslint',
         include: paths.appSrc,
       },
       {
@@ -142,14 +132,7 @@ module.exports = {
           {
             test: /\.(js|jsx|mjs)$/,
             include: paths.appSrc,
-            loader: require.resolve('babel-loader'),
-            options: {
-              
-              // This is a feature of `babel-loader` for webpack (not Babel itself).
-              // It enables caching results in ./node_modules/.cache/babel-loader/
-              // directory for faster rebuilds.
-              cacheDirectory: true,
-            },
+            use: 'happypack/loader?id=jsx',
           },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
@@ -158,36 +141,13 @@ module.exports = {
           // in development "style" loader enables hot editing of CSS.
           {
             test: /\.css$/,
-            use: [
-              require.resolve('style-loader'),
-              {
-                loader: require.resolve('css-loader'),
-                options: {
-                  importLoaders: 1,
-                },
-              },
-              {
-                loader: require.resolve('postcss-loader'),
-                options: {
-                  // Necessary for external CSS imports to work
-                  // https://github.com/facebookincubator/create-react-app/issues/2677
-                  ident: 'postcss',
-                  plugins: () => [
-                    require('postcss-flexbugs-fixes'),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }),
-                  ],
-                },
-              },
-            ],
+            use: 'happypack/loader?id=css',
           },
+          {
+            test: /\.(sass|scss)$/,
+            use: 'happypack/loader?id=sass',
+          },
+
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
@@ -211,6 +171,86 @@ module.exports = {
     ],
   },
   plugins: [
+    new HappyPack({
+      id: 'eslint',
+      threadPool: threadPool,
+      loaders: [
+        {
+          options: {
+            formatter: eslintFormatter,
+            eslintPath: require.resolve('eslint'),
+            fix: true,
+          },
+          loader: require.resolve('eslint-loader'),
+        },
+      ],
+    }),
+    new HappyPack({
+      id: 'jsx',
+      threadPool: threadPool,
+      loaders: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            // This is a feature of `babel-loader` for webpack (not Babel itself).
+            // It enables caching results in ./node_modules/.cache/babel-loader/
+            // directory for faster rebuilds.
+            cacheDirectory: true,
+          },
+        },
+      ],
+    }),
+    new HappyPack({
+      id: 'css',
+      threadPool: threadPool,
+      loaders: [
+        require.resolve('style-loader'),
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            importLoaders: 1,
+          },
+        },
+        {
+          loader: require.resolve('postcss-loader'),
+        },
+      ],
+    }),
+    new HappyPack({
+      id: 'sass',
+      threadPool: threadPool,
+      loaders: [
+        {
+          loader: require.resolve('style-loader'),
+        },
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            sourceMap: true,
+            modules: true,
+            localIdentName: '[name]_[local]_[hash:base64:6]',
+            camelCase: true,
+          },
+        },
+        {
+          loader: require.resolve('postcss-loader'),
+        },
+        {
+          loader: require.resolve('resolve-url-loader'),
+          options: {
+            sourceMap: false,
+          },
+        },
+        {
+          loader: require.resolve('sass-loader'),
+          options: {
+            sourceMap: true,
+            errLogToConsole: true,
+          },
+        },
+      ],
+    }),
+
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  sourceMap: true,
+  ident: "postcss",
+  plugins: [
+    require("postcss-flexbugs-fixes"),
+    autoprefixer({
+      browsers: [
+        ">1%",
+        "last 4 versions",
+        "Firefox ESR",
+        "not ie < 9" // React doesn't support IE8 anyway
+      ],
+      flexbox: "no-2009"
+    })
+  ]
+};
